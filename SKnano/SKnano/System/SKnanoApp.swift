6611c42c57dd19fc3249cfa826b0a62e06ffa8af
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-11-06 17:39:24 -0300

    Create Base project

---
+++ SKnano/SKnano/System/SKnanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKnanoApp.swift
+//  SKnano
+//
+//  Created by Eduardo on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SKnanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
